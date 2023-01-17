ecb1b26f075ee2b038543467fcf4e68a1900a2e3
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-17 20:05:06 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from config import BOT_TOKEN, APP_ID, API_HASH, FILM_DEPO
+import random
+
+bot = Client("RandomFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+message_id = random.randint(4, 1456)
+
+@Client.on_message(filters.command("film") & filters.private)
+async def film(bot, message):
+    try:
+        await bot.copy_message(
+            chat_id=chat_id, 
+            from_chat_id=PRE_LOG, 
+            message_id=message_id)
+    except Exception as e:
+        print(e)
+        bot.send_message(chat_id, "bir hata oluştu")
+      
+bot.run()
