b08f72abf9d137046cff5492d0826a547230aa78
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-17 20:26:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,11 @@ message_id = random.randint(4, 1456)
 async def film(bot, message):
     try:
         await bot.copy_message(
-            chat_id=chat_id, 
+            chat_id=message.chat_id, 
             from_chat_id=FILM_DEPO, 
             message_id=message_id)
     except Exception as e:
         print(e)
-        bot.send_message(chat_id, "bir hata oluştu")
+        bot.send_message(message.chat_id, "bir hata oluştu")
       
 Bot.run()
