a4c8280149a66e6c60ac74e14c4f42f81b6b2aaf
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-01-17 20:23:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ from pyrogram import Client, filters
 from config import BOT_TOKEN, APP_ID, API_HASH, FILM_DEPO
 import random
 
-bot = Client("RandomFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+Bot = Client("RandomFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 message_id = random.randint(4, 1456)
 
-@Client.on_message(filters.command("film") & filters.private)
+@Bot.on_message(filters.command("film") & filters.private)
 async def film(bot, message):
     try:
         await bot.copy_message(
