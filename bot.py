33e501c819b4fd50d592132cfa936b850e4799ce
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-05 17:19:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from config import BOT_TOKEN, APP_ID, API_HASH, FILM_DEPO
 import random
 
+
 Bot = Client("RanmFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
@@ -14,19 +15,20 @@ async def copy(bot, message, message_id, text):
             from_chat_id=FILM_DEPO, 
             message_id=message_id)
         await text.delete()
-        await filmsira(bot, message)
+        
+        await filmdongu(bot, message)
     except Exception as e:
         print(e)
         await bot.send_message(message.chat.id, "bir hata oluştu")
 
 
-async def filmsira(bot, message):
+async def filmdongu(bot, message):
     try:
-        message_id2 = 153
-        message_id = message_id2 + 1
+        message_id = [153]
         text = await bot.send_message(
             chat_id=message.chat.id,
             text="işlem Yapıyom")
+        message_id.append(message)
         await copy(bot, message, message_id, text)
     except Exception as e:
         print(e)
@@ -34,11 +36,7 @@ async def filmsira(bot, message):
 @Bot.on_message(filters.command("film") & filters.private)
 async def filmg(bot, message):
     try:
-        message_id = 153
-        text = await bot.send_message(
-            chat_id=message.chat.id,
-            text="işlem Yapıyom")
-        await copy(bot, message, message_id, text)
+        await filmdongu(bot, message)
     except Exception as e:
         print(e)
    
