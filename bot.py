bb53dafbacafeb62970d4bb48e0d548a61d0c3dc
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-05 16:49:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,16 @@
 from pyrogram import Client, filters
-from config import BOT_TOKEN, APP_ID, API_HASH, FILM_DEPO, STRING_SESSION
+from config import BOT_TOKEN, APP_ID, API_HASH, FILM_DEPO
 import random
 
 Bot = Client("RanmFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-userbot = Client("FilmBot", api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION)
 
 @Bot.on_message(filters.command("film") & filters.private)
 async def film(bot, message):
     try:
-        get_chat = await userbot.get_chat(FILM_DEPO)
+        get_chat = await bot.get_chat(FILM_DEPO)
         print(get_chat.title)
         message_id = random.randint(500, 1083)
-        await userbot.copy_message(
+        await bot.copy_message(
             chat_id=message.chat.id, 
             from_chat_id=FILM_DEPO, 
             message_id=message_id)
