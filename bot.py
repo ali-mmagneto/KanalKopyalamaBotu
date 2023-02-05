dd6386d4e3e1a41fe4460841df69d7010ac634bc
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-05 18:05:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,14 +6,14 @@ import random
 Bot = Client("RanmFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
-async def copy(bot, message, text, message_id):
+async def copy(bot, message, text, id):
     try:
         get_chat = await bot.get_chat(FILM_DEPO)
         print(get_chat.title)
         await bot.copy_message(
             chat_id=message.chat.id, 
             from_chat_id=FILM_DEPO, 
-            message_id=message_id)
+            message_id=id)
         await text.delete()
         await filmdongu(bot, message)
     except Exception as e:
@@ -21,20 +21,20 @@ async def copy(bot, message, text, message_id):
         await bot.send_message(message.chat.id, f"bir hata oluştu `{e}`")
 
 
-async def filmdongu(bot, message, message_id):
+async def filmdongu(bot, message, id):
     try:
         text = await bot.send_message(
             chat_id=message.chat.id,
             text="işlem Yapıyom")
-        await copy(bot, message, text, message_id)
+        id = 152
+        id += 1
+        await copy(bot, message, text, id)
     except Exception as e:
         print(e)
 
 @Bot.on_message(filters.command("film") & filters.private)
 async def filmg(bot, message):
     try:
-        message_id = [152]
-        mesaage_id += 1
         await filmdongu(bot, message, message_id)
     except Exception as e:
         print(e)
