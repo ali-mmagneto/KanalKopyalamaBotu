8ef6f144378c95a2c93b8e9a22b288fd634242d4
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-05 16:41:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 from os import environ
 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+STRING_SESSION = environ.get("STRING_SESSION", "")
 FILM_DEPO = environ.get("FILM_DEPO", "") 
 APP_ID = int(environ.get("APP_ID", 1234))
 API_HASH = environ.get("API_HASH", "")
