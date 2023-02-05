8993d6304a572eb22672869d12c4adb075dfc6ab
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-05 17:28:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ async def copy(bot, message, message_id, text):
         await bot.copy_message(
             chat_id=message.chat.id, 
             from_chat_id=FILM_DEPO, 
-            message_id=int(message_id))
+            message_id=message_id)
         await text.delete()
         await filmdongu(bot, message)
     except Exception as e:
@@ -23,12 +23,12 @@ async def copy(bot, message, message_id, text):
 
 async def filmdongu(bot, message):
     try:
-        message_id = (153) 
+        message_id = 153
         text = await bot.send_message(
             chat_id=message.chat.id,
             text="işlem Yapıyom")
-        message_id.append(message)
         await copy(bot, message, message_id, text)
+        mesaage_id = message_id + 1
     except Exception as e:
         print(e)
 
