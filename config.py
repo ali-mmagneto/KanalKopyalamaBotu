9e5dcd5eb95e0f675973345fb1edc8bd60c68a36
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-06 16:29:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,9 @@ import pyrogram
 from pyrogram import Client
 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-STRING_SESSION = environ.get("STRING_SESSION", "")
 FILM_DEPO = environ.get("FILM_DEPO", "") 
 DEPO = environ.get("DEPO", "")
 APP_ID = int(environ.get("APP_ID", 1234))
 API_HASH = environ.get("API_HASH", "")
 SON_MSG_ID = int(environ.get("SON_MSG_ID", ""))
 ILK_MSG_ID = int(environ.get("ILK_MSG_ID", ""))
-userbot = Client("RanmuserBot", api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION)
-userbot.start()
