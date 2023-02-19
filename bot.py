fcd7cf2a15e283f2c002c60755ab7c680e72321a
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-19 22:42:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import asyncio
 
 Bot = Client("RanmFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-async def copy(bot, message, id):
+async def copy(bot, message, id, son_id):
     try:
-        if id > SON_MSG_ID:
+        if id > son_id:
             await bot.send_message(message.chat.id, "`İşlem Tamamlandı`")
         else:
             film_kanal = await bot.get_chat(chat_id=FILM_DEPO)
@@ -21,22 +21,28 @@ async def copy(bot, message, id):
         print(e)
         await bot.send_message(message.chat.id, f"bir hata oluştu `{e}`")
 
-async def filmdongu(bot, message, id):
+async def filmdongu(bot, message, id, son_id):
     try:
         id += 1
         await asyncio.sleep(5)
-        await copy(bot, message, id)
+        await copy(bot, message, id, son_id)
     except Exception as e:
         print(e)
 
 @Bot.on_message(filters.command("film") & filters.private)
 async def filmg(bot, message):
     try:
-        id = ILK_MSG_ID
+        text = message.text.split(" ", 1)
+        if len(text) < 4:
+            await message.reply_text("Hatalı Kullanım")
+            return
+        from_kanal = text[1 ]
+        id = text[2]
+        son_id = text[3]
         text = await bot.send_message(
             chat_id=message.chat.id,
             text="`Filmleri Kopyalıyorum Bekle`")
-        await filmdongu(bot, message, id)
+        await filmdongu(bot, message, id, son_id)
     except Exception as e:
         print(e)
     
