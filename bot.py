cea35f832f5b3e6ab13267d79b740f6da67900c5
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-19 22:44:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,31 @@
 from pyrogram import Client, filters
-from config import BOT_TOKEN, APP_ID, API_HASH, FILM_DEPO, DEPO, SON_MSG_ID, ILK_MSG_ID
+from config import BOT_TOKEN, APP_ID, API_HASH, DEPO
 import random
 import asyncio
 
 Bot = Client("RanmFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-async def copy(bot, message, id, son_id):
+async def copy(bot, message, id, son_id, kanal_id):
     try:
         if id > son_id:
             await bot.send_message(message.chat.id, "`İşlem Tamamlandı`")
         else:
-            film_kanal = await bot.get_chat(chat_id=FILM_DEPO)
+            film_kanal = await bot.get_chat(chat_id=kanal_id)
             print(film_kanal)
             await bot.copy_message(
                 chat_id=DEPO, 
-                from_chat_id=FILM_DEPO, 
+                from_chat_id=kanal_id, 
                 message_id=id)
             await filmdongu(bot, message, id)
     except Exception as e:
         print(e)
         await bot.send_message(message.chat.id, f"bir hata oluştu `{e}`")
 
-async def filmdongu(bot, message, id, son_id):
+async def filmdongu(bot, message, id, son_id, kanal_id):
     try:
         id += 1
         await asyncio.sleep(5)
-        await copy(bot, message, id, son_id)
+        await copy(bot, message, id, son_id, kanal_id)
     except Exception as e:
         print(e)
 
@@ -36,13 +36,13 @@ async def filmg(bot, message):
         if len(text) < 4:
             await message.reply_text("Hatalı Kullanım")
             return
-        from_kanal = text[1 ]
+        kanal_id = text[1 ]
         id = text[2]
         son_id = text[3]
         text = await bot.send_message(
             chat_id=message.chat.id,
             text="`Filmleri Kopyalıyorum Bekle`")
-        await filmdongu(bot, message, id, son_id)
+        await filmdongu(bot, message, id, son_id, kanal_id)
     except Exception as e:
         print(e)
     
