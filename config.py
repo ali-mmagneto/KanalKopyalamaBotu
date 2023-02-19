5fb1aea7312ff9bbfde8850e3035aa0667c7fb7c
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-19 22:42:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,7 @@ from os import environ
 import pyrogram
 from pyrogram import Client
 
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-FILM_DEPO = environ.get("FILM_DEPO", "") 
+BOT_TOKEN = environ.get("BOT_TOKEN", "") 
 DEPO = environ.get("DEPO", "")
 APP_ID = int(environ.get("APP_ID", 1234))
 API_HASH = environ.get("API_HASH", "")
-SON_MSG_ID = int(environ.get("SON_MSG_ID", ""))
-ILK_MSG_ID = int(environ.get("ILK_MSG_ID", ""))
