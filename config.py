bad3d446ad53ef8cf9fb329300d07d409aac76a6
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-23 20:52:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,10 @@ from os import environ
 import pyrogram
 from pyrogram import Client
 
-BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+STRING_SESSION = environ.get("STRING_SESSION", "") 
 DEPO = environ.get("DEPO", "")
 APP_ID = int(environ.get("APP_ID", 1234))
 API_HASH = environ.get("API_HASH", "")
+userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
+userbot.start()
