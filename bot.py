e49ad8ad81b9f95d57fd7c0c549829800b2a4c27
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-23 21:18:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,70 @@ from config import BOT_TOKEN, APP_ID, API_HASH, DEPO, userbot
 import random
 import asyncio
 from unidecode import unidecode
+import time
 
 Bot = Client("RanmFilmBot", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+async def progress_bar(current, total, text, message, start):
+
+    now = time.time()
+    diff = now-start
+    if round(diff % 10) == 0 or current == total:
+        percentage = current*100/total
+        speed = current/diff
+        elapsed_time = round(diff)*1000
+        eta = round((total-current)/speed)*1000
+        ett = eta + elapsed_time
+
+        elapsed_time = TimeFormatter(elapsed_time)
+        ett = TimeFormatter(ett)
+
+        progress = "[{0}{1}] \n**İlerleme**: {2}%\n".format(
+            ''.join(["●" for i in range(math.floor(percentage / 10))]),
+            ''.join(["○" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2))
+
+        tmp = progress + "**Indirilen**: {0}/{1}\n**Hız**: `{2}`/s\n**Tahmini Süre**: `{3}`\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            # elapsed_time if elapsed_time != '' else "0 s",
+            ett if ett != '' else "0 s"
+        )
+
+        try :
+            await message.edit(
+                text = '{}'.format(tmp)
+            )
+        except:
+            pass
+
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
+
 
 async def copy(bot, message, id, son_id, kanal_id):
     try:
