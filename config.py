2d341ca0334258904a7ec63ac34f29f02d76c72b
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-02-23 22:14:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ STRING_SESSION = environ.get("STRING_SESSION", "")
 DEPO = environ.get("DEPO", "")
 APP_ID = int(environ.get("APP_ID", 1234))
 API_HASH = environ.get("API_HASH", "")
+DOWNLOAD_DIR = 'downloads'
 userbot = Client(name='userbot', api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION, parse_mode=enums.ParseMode.HTML)
 userbot.start()
